d480bef512508a0a28a9509c200b22eea83da418
Julia <juliagreis.ti@gmail.com>
2025-06-10 18:40:22 -0300

    implementacao do operador de pre incremento e demais modificacoes necessárias para que ele funcione (inset,find,copia nodes)..., implementacao do construtor de copia e correcoes em funcoes que apresentaram pequenos problemas

--- set_arvoreBPM_nao_balanceada/Set_bpm.hpp
+++ set_arvoreBPM_nao_balanceada/Set_bpm.hpp
@@ -5,8 +5,8 @@
 template  <class T>
 class Node {
 	public: //classe auxiliar.. vamos utiliza-la apenas neste arquivo (nao e' muito necessario ter encapsulamento)
-		Node(const T &elem_) : elem(elem_),left(NULL), right(NULL) {}
-		Node<T> *left, *right;
+		Node(const T &elem_) : elem(elem_),left(NULL), right(NULL), parent(NULL) {}
+		Node<T> *left, *right, *parent;
 		T elem;
 };
 
@@ -26,7 +26,44 @@ class Set_BPM_iterator{
         T& operator*(){return ptr->elem;};
         bool operator==(const Set_BPM_iterator &other){return ptr==other.ptr;}
         bool operator!=(const Set_BPM_iterator &other){return !(ptr==other.ptr);}
+        Set_BPM_iterator& operator++();  //nao recebe nada pois irá """somar""" 1 na memoria
+	    Set_BPM_iterator& operator--();
 };
+template<class T>
+Set_BPM_iterator<T>& Set_BPM_iterator<T>::operator++(){
+	//o menor elemento maior que ele, logo:
+	if(ptr->right){
+		ptr=ptr->right;
+		//agora encontramos o menor (ou seja, mais a esquerda)
+		while(ptr->left) ptr=ptr->left;
+	}
+	else{ // se nao tiver ninguem a direita dele, entao
+		while(ptr->parent && ptr==ptr->parent->right){
+			ptr=ptr->parent;
+		}
+		//suba + 1 vezes
+		ptr=ptr->parent;
+	}
+	return *this;
+}
+
+template<class T>
+Set_BPM_iterator<T>& Set_BPM_iterator<T>::operator--(){
+	//o menor elemento maior que ele, logo:
+	if(ptr->left){
+		ptr=ptr->left;
+		//agora encontramos o menor (ou seja, mais a esquerda)
+		while(ptr->right) ptr=ptr->right;
+	}
+	else{ // se nao tiver ninguem a direita dele, entao
+		while(ptr->parent && ptr==ptr->parent->left){
+			ptr=ptr->parent;
+		}
+		//suba + 1 vezes
+		ptr=ptr->parent;
+	}
+	return *this;
+}
 
 
 
@@ -39,16 +76,17 @@ class Set_BPM{
     public:
         typedef Set_BPM_iterator<T> iterator;
         Set_BPM();
+        Set_BPM(const Set_BPM &other);
         ~Set_BPM();
 
-        iterator begin(){return iterator(root);}
+        iterator begin();
         iterator end(){return iterator(NULL);}
 
 
         std::pair<iterator,bool> insert(const T& elem);
         iterator find(const T& elem);
 
-        Node<T> copiaNodes(Node<T> * root) const;
+        Node<T>* copiaNodes(Node<T> * root,Node<T> *parent) const;
         Set_BPM &operator=(const Set_BPM &other);
 
         //bfs e dfs
@@ -77,7 +115,7 @@ class Set_BPM{
         T elem;
         int size;
         //funcoes auxiliares
-        std::pair<iterator,bool>insert_recursion(const T& elem, Node<T> * &root);
+        std::pair<iterator,bool>insert_recursion(const T& elem, Node<T> * &root, Node<T> *parent);
         iterator find_recursion(const T& elem, Node<T> * &root);
         void delete_recursion(Node<T> *root);
 
@@ -90,6 +128,14 @@ class Set_BPM{
 
 template <class T>
 Set_BPM<T>::Set_BPM(){ root=NULL; size=0;}
+
+template <class T>
+Set_BPM<T>::Set_BPM(const Set_BPM &other){
+    root = NULL;
+    size = 0;
+    root = copiaNodes(other.root, NULL);
+    size = other.size;
+}
 
 template <class T>
 void Set_BPM<T>::delete_recursion(Node<T> *root){
@@ -105,29 +151,38 @@ Set_BPM<T>::~Set_BPM() {
     delete_recursion(root);
 }
 
+template <class T>
+Set_BPM_iterator<T> Set_BPM<T>::begin(){
+    if (root==NULL) return end();
+    Node<T> *aux=root;
+    while(aux->left) aux=aux->left;
+    return iterator(aux);
+}
+
 template<class T>
-std::pair<typename Set_BPM<T>::iterator,bool> Set_BPM<T>::insert_recursion(const T& elem, Node<T> * &root){
+std::pair<typename Set_BPM<T>::iterator,bool> Set_BPM<T>::insert_recursion(const T& elem, Node<T> * &root, Node<T> *parent){
     if(!root){
         size++;
         root=new Node<T>(elem);
+        root->parent=parent;
         return std::make_pair(iterator(root), true);
     }
 
-    if(elem < root->elem) return insert_recursion(elem,root->left);
-    else if(elem>root->elem) return insert_recursion(elem,root->right);
+    if(elem < root->elem) return insert_recursion(elem,root->left,root);
+    else if(elem>root->elem) return insert_recursion(elem,root->right,root);
     else return std::make_pair(iterator(root), false);
 }
 
 template<class T>
 std::pair<typename Set_BPM<T>::iterator,bool> Set_BPM<T>::insert(const T& elem){
-    return insert_recursion(elem, root);
+    return insert_recursion(elem, root,NULL);
 }
 
 template<class T>
 typename Set_BPM<T>::iterator Set_BPM<T>:: find_recursion(const T& elem, Node<T> * &root){
     if(!root) return iterator(NULL);
-    else if(root->left && root->elem < elem) return find_recursion(elem,root->right);
-    else if(root->right && root->elem > elem) return find_recursion(elem,root->left);
+    else if(root->elem < elem) return find_recursion(elem,root->right);
+    else if(root->elem > elem) return find_recursion(elem,root->left);
     return iterator(root);
 }
 
@@ -137,12 +192,13 @@ typename Set_BPM<T>::iterator Set_BPM<T>::find(const T& elem){
 }
 
 template <class T>
-Node<T> Set_BPM<T>::copiaNodes(Node<T> * root)const {
-    if(root==NULL) return root;
+Node<T>* Set_BPM<T>::copiaNodes(Node<T> * root,Node<T> *parent)const {
+    if(root==NULL) return NULL;
 
-    Node<T> aux=new Node<T>(root->elem); //fiz um node de mesmo valor
-    aux->left=copiaNodes(root->left);
-    aux->right=copiaNodes(root->right);
+    Node<T> *aux=new Node<T>(root->elem); //fiz um node de mesmo valor
+    aux->parent=parent;
+    aux->left=copiaNodes(root->left,aux);
+    aux->right=copiaNodes(root->right,aux);
 
     return aux;
 }
@@ -152,8 +208,11 @@ Set_BPM<T>& Set_BPM<T>::operator=(const Set_BPM<T> &other){
     //verificamos se estamos tentando uma auto-atibuicao
     if(this==&other) return *this;
     //garantimos que other está vazio
-    delete_recursion(other.root);
-    root=copiaNodes(other.root);
+    delete_recursion(root);
+    root = NULL;
+    size = 0;
+    
+    root=copiaNodes(other.root,NULL);
     size=other.size;
     return *this;
 
--- set_arvoreBPM_nao_balanceada/main_teste.cpp
+++ set_arvoreBPM_nao_balanceada/main_teste.cpp
@@ -1,35 +1,71 @@
 #include <iostream>
-#include "Set_bpm.hpp"  // ajuste o caminho conforme necessário
+#include "Set_bpm.hpp" 
+
+template <class T>
+void imprime(Set_BPM<T> &arvore){
+    Set_BPM_iterator<T> b = arvore.begin();
+    Set_BPM_iterator<T> e = arvore.end();
+    while(b != e){
+        std::cout << *b << " ";
+        ++b;
+    }
+    std::cout << std::endl;
+}
 
 int main() {
     Set_BPM<int> conjunto;
 
-    std::pair<Set_BPM<int>::iterator, bool> resultado1 = conjunto.insert(10);
-    std::pair<Set_BPM<int>::iterator, bool> resultado2 = conjunto.insert(5);
-    std::pair<Set_BPM<int>::iterator, bool> resultado3 = conjunto.insert(15);
-    std::pair<Set_BPM<int>::iterator, bool> resultado4 = conjunto.insert(10); // valor repetido
+    // Inserções
+    conjunto.insert(10);
+    conjunto.insert(5);
+    conjunto.insert(15);
     conjunto.insert(2);
     conjunto.insert(8);
-    conjunto.insert(24);
+    conjunto.insert(12);
+    conjunto.insert(20);
     conjunto.insert(3);
 
-    if (resultado1.second) std::cout << "10 inserido\n";
-    else std::cout << "10 ja existia\n";
+    // Inserção duplicada
+    std::pair<Set_BPM<int>::iterator, bool> resultado = conjunto.insert(10);
+    if (!resultado.second) {
+        std::cout << "Elemento 10 repetido, nao inserido\n";
+    }
 
-    if (resultado4.second) std::cout << "10 inserido novamente\n";
-    else std::cout << "10 repetido, nao inserido\n";
-
-    Set_BPM<int>::iterator it = conjunto.find(5);
+    // Busca existente
+    Set_BPM<int>::iterator it = conjunto.find(8);
     if (it != conjunto.end()) {
         std::cout << "Encontrado: " << *it << std::endl;
     } else {
-        std::cout << "5 nao encontrado\n";
+        std::cout << "8 nao encontrado\n";
     }
 
+    // Busca inexistente
     Set_BPM<int>::iterator it2 = conjunto.find(99);
     if (it2 == conjunto.end()) {
         std::cout << "99 nao encontrado\n";
     }
+
+    // Percursos
+    std::cout << "\nDFS - Pre-ordem: ";
+    conjunto.imprimeDFS_pre_order();
+    std::cout << "\nDFS - Em-ordem: ";
+    conjunto.imprimeDFS_in_order();
+    std::cout << "\nDFS - Pos-ordem: ";
     conjunto.imprimeDFS_pos_order();
+
+    std::cout << "\n\nBFS: \n";
+    conjunto.imprimeBFS();
+
+    // Iteração com imprime()
+    std::cout << "\nIterando com imprime(): ";
+    imprime(conjunto);
+
+    // Teste de cópia
+    Set_BPM<int> copia;
+    copia = conjunto;
+
+    std::cout << "Cópia da árvore com imprime(): ";
+    imprime(copia);
+
     return 0;
 }
